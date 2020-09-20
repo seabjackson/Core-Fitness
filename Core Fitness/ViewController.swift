5b80f1c0f3389ceac2efa2c5540443a86598718b
Seab <honeyboyseab@gmail.com>
2020-09-20 12:57:23 -0400

    Initial Commit

---
+++ Core Fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Fitness
+//
+//  Created by Seab Jackson on 9/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
