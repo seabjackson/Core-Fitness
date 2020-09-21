04a0ca03ded4e7b47648e6202b97f4671f303cf4
Seab <honeyboyseab@gmail.com>
2020-09-21 01:58:27 -0400

    Delivers unexpected values in the event that a request completes with data, error and response as nil values

--- Core Fitness/Model/ClassFeed API/URLSessionHTTPClient.swift
+++ Core Fitness/Model/ClassFeed API/URLSessionHTTPClient.swift
@@ -15,11 +15,15 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedValues: Error {}
+    
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValues()))
             }
-        }
+        }.resume()
     }
 }
